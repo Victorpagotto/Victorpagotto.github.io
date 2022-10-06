d28df89888bb753fc3b7ba4a68752567dafc352a
victorpagotto <victor.pagotto12@gmail.com>
2022-10-05 23:13:36 -0300

    Adicionados projetos 'Portfolio' e 'Store Manager'.

--- funcs/data/projectData.js
+++ funcs/data/projectData.js
@@ -8,7 +8,38 @@ const projectFields = {
   },
   descript: ['DESCRIÇÃO', 'DESCRIPTION'],
 }
+
+// Portfolio
+
+const portfolio = {
+  params: {
+    name: ['Portfólio', 'Portfolio'],
+    knowledge: ['JavaScript, HTML e CSS.', 'JavaScript, HTML and CSS.'],
+    tools: ['ESLint.', 'ESLint.'],
+    party: ['1 (sozinho).', '1 (solo).'],
+    github: ['https://github.com/Victorpagotto/portfolio', 'https://github.com/Victorpagotto/portfolio'],
+  },
+  text: [[
+    ['Criado com o propósito de documentar meu trabalho e meus projetos, a intenção era de criar algo simples e direto, onde eu apenas mostrasse o que sei fazer e então fornecesse projetos para demonstrar isso de forma documentada o que fiz e pretendo fazer. Para esse propósito, criei apenas duas páginas; uma na qual eu me apresentaria e diria informações sobre mim, minhas experiências e objetivos, e a outra onde eu mostraria os projetos com textos em aspecto de registro do que foi feito.'],
+    ['Toda a página deve funcionar em duas línguas: português e inglês, as quais são as duas que domino. Entretanto o código é capaz de comportar um número ilimitado de linguagens, desde que a informação necessária seja fornecida. Para tal, todo o site não possui texto em seu documento HTML, e, ao invés disso toda a informação é alimentada através de JavaScript através de um gerenciador de informações para cada página. A informação é passada como um objeto composto por dois arrays em cada chave, e a posição do array determina qual língua é selecionada e visualizada, sendo 0 português e 1 inglês; a partir disso, é apenas necessário intercalar o valor da variável de linguagenm para a mudar.'],
+    ['Este sistema de gerenciamento de informações é muito mais pesadamente aplicado na página de projetos. Através de código, a página é gerada de acordo com qual projeto foi selecionado pelo usuário, criando assim uma biblioteca que pode ser ampliada sem a necessidade de se estender a página em si, como numa interface. Apesar de limitar a edição da página e da documentação da criação do projeto a seções pré-estabelecidas, atinge completamente a meta de criar algo simples mas capaz de registrar corretamente o meu trabalho.'],
+    ['A estrutura html acabou por possuir uma grande quantidade de div\'s. O motivo disso é a criação de elementos com mero propósito estético, os quais são div\'s posicionadas de forma a gerar formas específicas através de intermediárias com uma mistura de posicionamento absoluto e relativo. Através disso foi possível gerar formas, como as da seção de conhecimentos ou o enquadramento da foto.'],
+    ['A página foi projetada para ser completamente responsiva, podendo ser usada em todos os tamanhos de tela. Para atingir isso entretanto houve um desafio a ser superado: a barra de menu. Por possuir uma animação a partir de intervalo, a mudança no tamanho da página causava problemas em seus limites de movimento. Para solucionar isso, foi usado um observador, o qual iria informar o código a respeito de mudanças nas especificações da tela.'],
+    ['Com a estrutura da página pronta, objetivos futuros incluem adicionar mais dos projetos que fiz ao longo do último ano, e tornar os ícones da seção de conhecimentos na página principal selecionáveis, levando a um projeto aleatório na página de projetos no qual aquele conhecimento selecionado tenha sido usado.'],
+  ],
+  [
+    ['Created for documenting my work and projects, the intention was to create something simple and straightforward, with wich I just show what I know and how I know what to, do and then provide projects to demonstrate, in a documented manner, what I have done and intend to do in the future. For this purpose, I created only two pages; one in which I would introduce myself and tell information about myself, like my experiences and goals, and the other where I would show the projects with texts, as a record of what had been done.'],
+    ['The entire page works with two languages: Portuguese and English, which are the ones I know. However, the code itself is capable of an unlimited number of languages, as long as the necessary data is provided. To do this, the entire site does not have text in its HTML document, instead all the information is fed via JavaScript using an information manager for each page. The information is passed as an object composed of two arrays in each key, and the position of the array determines which language is selected and displayed, 0 being Portuguese and 1 English; from it, it is only necessary to swap the value of the language variable for it to change.'],
+    ['This data management system is more heavily applied on the projects page. Using code, the page is generated according to which project was selected by the user, thus creating a library that can be expanded without the need to extend the page itself, like an interface. Despite limiting page editing and project creation documentation to pre-established sections, it completely achieves the goal of creating something simple but capable of correctly registering my work.'],
+    ['The html structure ended up having a lot of div\'s. The reason behind it is the creation of elements with a mere aesthetic purpose, which are div\'s positioned to generate specific shapes through intermediates with a mix of absolute and relative positioning. Through this it was possible to generate shapes, such as those in the knowledge section or the framing of the photo.'],
+    ['The page is designed to be fully responsive and can be used on all screen sizes. To achieve this, however, there was a challenge to be overcome: the menu bar. Because it has an interval animation, changing the size of the page caused problems in its limits of movement. To solve this, an observer was created, which would give data to the code about changes in the screen specifications.'],
+    ['With the page structure already done, future goals include adding more of the projects I\'ve done over the last year, and making the icons of the knowledges section on the main page selectable, which will lead to a random project on the projects page in which that specific knowledge has been used in.'],
+  ],],
+  gifs: [],
+}
+
 // RECIPE APP
+
 const recipeApp = {
   params: {
     name: ['Cook a Cookie', 'Cook a Cookie'],
@@ -39,8 +70,8 @@ const blogAPI = {
     name: ['Blog API', 'Blog API'],
     knowledge: ['JavaScript e Sequelize.', 'JavaScript and Sequelize.'],
     tools: ['Node.js, JWT, Express e MySQL.', 'Node.js, JWT, Express and MySQL.'],
-    party: ['1 (sozinho)', '1 (solo)'],
-    github: ['https://github.com/Victorpagotto/blog-api-project/blob/main/README.md', 'https://github.com/Victorpagotto/blog-api-project/blob/main/README.md'],
+    party: ['1 (sozinho).', '1 (solo).'],
+    github: ['https://github.com/Victorpagotto/blog-api-project', 'https://github.com/Victorpagotto/blog-api-project'],
   },
   text: [[
     ['Desenvolvida como uma API capaz de suportar um blog, esta possui a capacidade de receber, devolver, pesquisar e manejar (CRUD) informações sobre usuários, posts e categorias em um banco de dados MySQL utilizando-se da abstração proveniente do Sequelize, uma ferramenta capaz de servir de interface entre o código da API e o banco de daods, com modelos de query prontos. Dito isso, a API é focada em desenvolver os serviços e controladores com base no Express e suas rotas.'],
@@ -54,11 +85,39 @@ const blogAPI = {
   ],],
   gifs: [],
 }
+
+// Store Manager
+
+const storeManager = {
+  params: {
+    name: ['Projeto Store Manager', 'Store Manager Project'],
+    knowledge: ['JavaScript e MySQL.', 'JavaScript and MySQL.'],
+    tools: ['Node.js, Express, chai, sinon e MySQL.', 'Node.js, Express, chai, sinon and MySQL.'],
+    party: ['1 (sozinho).', '1 (solo).'],
+    github: ['https://github.com/Victorpagotto/-store-manager-project', 'https://github.com/Victorpagotto/-store-manager-project'],
+  },
+  text: [[
+    ['Esta é uma aplicação construída com base na estrutura MSC: model, service e controller. Construído sobre a ideia de dropshipping, é possível com ela realizar o CRUD: criar, ver, mudar e apagar produtos e vendas. A gestão de dados acontece através de um banco de dados MySQL, com as queries devidamente criadas dentro dos models. Além disso, tendo sido criada dentro dos princípios de arquitetura REST, a aplicação mantém a integridade de recebimento, manejo e resposta de dados.'],
+    ['A separação dos elementos da aplicação em camadas garante uma maior facilidade de atualização em caso de necessidade, pois o código encarregado de cada categoria de tarefas é desacoplado. Por exemplo, mudar o banco de dados causaria mudanças apenas na camada de models, e uma mudança de regra de negócios apenas afetaria a de service.'],
+    ['Este projeto conta também com testes unitários para cada camada e função dentro da estrutura MSC, garantindo uma maior integridade de código. Estes testes são feitos utilizando-se das ferramentas chai e sinon para se realizar o mock e a verificação da integridade.'],
+    ['Tendo sido o primeiro projeto feito por mim na estrutura MSC, o maior desafio foi visualizar as nuâncias e precisões no que tange as diferenças entre as camadas e a exata função de cada uma delas, algo resolvido através do pensamento abstrato a respeito do que deveria ou não ser afetado de acordo a possível necessidade de uma mudança no código no futuro, e como isso deveria ou não afetar cada camada.'],
+  ],
+  [
+    ['This is an application built on MSC: model, service and controller. Made with the idea of dropshipping, it is capable of CRUD: create, view, change and delete, of products and sales. Data is managed using a MySQL database, with queries properly created within the models. In addition, it has been created within the principles of REST architecture, thus the application maintains the integrity of receiving, handling and responding data.'],
+    ['The separation of the application\'s elements into layers guarantees better updating if necessary, as the code in charge of each task category is decoupled. For example, changing the database would only change the models layer, and a change of business rules would only affect the service layer.'],
+    ['This project also has unit testing for each layer and function within the MSC layers, ensuring greater code integrity. These tests are done using the chai and sinon tools for the mocking and integrity checking.'],
+    ['Being this the first project made by me in the MSC, the biggest challenge was to visualize the nuances and precisions regarding the differences between the layers and the exact functionality of each one of them, something solved through abstract thinking about what should or shouldn\'t be affected according to the possible need for a code change in the future, and how each layer would be affected by it.'],
+  ],],
+  gifs: [],
+}
+
 ///////////////////////////////////////
 
 const projectList = [
+  portfolio,
   recipeApp,
   blogAPI,
+  storeManager,
 ];
 
 const projectData = {
